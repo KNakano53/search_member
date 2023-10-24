f4c4fe17c4c08a6c072617037e8596b4d0602018
KNakano <knakano2@sompo-sys.com>
2023-10-24 11:29:47 +0900

    bootstrap用意

---
+++ frontend/src/searchBox.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import SearchResult from './SearchResult';
+import Button from 'react-bootstrap/Button';
+
+function searchBox() {
+  return (
+    <div className="App">
+      <div>
+        <input type='text' />
+
+      </div>
+    </div>
+  );
+}
+
+export default searchBox;
---
+++ frontend/src/searchResult.ts
@@ -0,0 +1,10 @@
+import React from "react";
+
+function searchResult(searchFlag :boolean){
+    if(!searchFlag){
+        return null;
+    }
+    
+}
+
+export default searchResult;
