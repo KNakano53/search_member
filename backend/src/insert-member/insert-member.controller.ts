d2ec4c3f3aa535a3d40768e83231fe9b202fc727
KNakano <knakano2@sompo-sys.com>
2024-01-09 14:38:40 +0900

    トランザクション管理修正

--- backend/src/insert-member/insert-member.controller.ts
+++ backend/src/insert-member/insert-member.controller.ts
@@ -1,22 +1,14 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { InsertUserDTO } from 'src/entity/user/insert.user.dto';
 import { InsertMemberService } from './insert-member.service';
-import { InjectEntityManager } from '@nestjs/typeorm';
-import { EntityManager } from 'typeorm';
 import { Response } from 'src/type/response.type';
 
 @Controller('insert-member')
 export class InsertMemberController {
-  constructor(
-    private readonly service: InsertMemberService,
-    @InjectEntityManager()
-    private readonly manager: EntityManager,
-  ) {}
+  constructor(private readonly service: InsertMemberService) {}
 
   @Post()
   async insertUser(@Body() body: InsertUserDTO): Promise<Response> {
-    return await this.manager.transaction(async (manager) => {
-      return await this.service.insertUser(body, manager);
-    });
+    return await this.service.insertUser(body);
   }
 }
--- backend/src/insert-member/insert-member.service.ts
+++ backend/src/insert-member/insert-member.service.ts
@@ -4,46 +4,56 @@ import { Response, generateResponse } from 'src/type/response.type';
 import { Users } from 'src/entity/user/users.entity';
 import { EntityManager } from 'typeorm';
 import { Sequence } from 'src/entity/user/sequence.entity';
+import { InjectEntityManager } from '@nestjs/typeorm';
 
 @Injectable()
 export class InsertMemberService {
-  async insertUser(
-    body: InsertUserDTO,
-    manager?: EntityManager,
-  ): Promise<Response> {
-    try {
-      const savedUser = await this.saveUser(body, manager);
-      return generateResponse(
-        [],
-        ['登録が完了しました', '新規加入者番号:' + savedUser.id],
-      );
-    } catch (e) {
-      console.log(e);
-      return generateResponse([], ['登録処理に失敗しました'], 400);
-    }
+  constructor(
+    @InjectEntityManager()
+    private readonly manager: EntityManager,
+  ) {}
+
+  async insertUser(body: InsertUserDTO): Promise<Response> {
+    return await this.manager.transaction(
+      async (transactionalEntityManager) => {
+        try {
+          const savedUser = await this.saveUser(
+            transactionalEntityManager,
+            body,
+          );
+          return generateResponse(
+            [],
+            ['登録が完了しました', '新規加入者番号:' + savedUser.id],
+          );
+        } catch (e) {
+          console.log(e);
+          return generateResponse([], ['登録処理に失敗しました'], 400);
+        }
+      },
+    );
   }
 
   private async saveUser(
+    entityManager: EntityManager,
     body: InsertUserDTO,
-    manager: EntityManager,
   ): Promise<Users> {
     const user: Users = body;
-    user.id = await this.generateUserID(manager);
-    return manager.getRepository(Users).save(user);
+    user.id = await this.generateUserID(entityManager);
+    return entityManager.getRepository(Users).save(user);
   }
 
-  private async getLastNum(manager: EntityManager): Promise<Sequence> {
-    const sequence = await manager
+  private async getLastNum(entityManager: EntityManager): Promise<Sequence> {
+    const sequence = await entityManager
       .getRepository(Sequence)
       .createQueryBuilder('sequence')
       .select('MAX(id)+1', 'id')
       .getRawOne<Sequence>();
 
-    return manager.getRepository(Sequence).save(sequence);
+    return entityManager.getRepository(Sequence).save(sequence);
   }
 
-  private async generateUserID(manager: EntityManager): Promise<string> {
-    const idNum: number = (await this.getLastNum(manager)).id;
+  private async generateUserID(entityManager: EntityManager): Promise<string> {
+    const idNum: number = (await this.getLastNum(entityManager)).id;
     const strHeader: string = 'TS';
     return strHeader + this.zeroPadding(idNum, 4);
   }
